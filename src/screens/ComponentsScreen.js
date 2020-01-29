2c00207cc644c514fc07a3591016702f3ab076ec
Anand Muraleedharan <anand.muraleedharan@gmail.com>
2020-01-29 15:25:39 -0600

    Section 2
    First Commit - Section 2 - Components

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {Text, StyleSheet, View} from 'react-native'
+
+const name = "Anand"
+
+const ComponentsScreen = () => {
+  return(
+    <View>
+      <Text style={styles.textStyle}>Getting started with React Native</Text>
+      <Text> My name is {name} </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  textStyle : {
+    fontSize: 45
+  }
+})
+
+export default ComponentsScreen
